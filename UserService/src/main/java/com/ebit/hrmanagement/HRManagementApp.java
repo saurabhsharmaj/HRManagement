dfb67827b5dbb978585f7fc732f24226b69cff93
saurabhsharmaj <saurabh.find@gmail.com>
2021-07-28 17:08:47 +0530

    update modules.

---
+++ UserService/src/main/java/com/ebit/hrmanagement/HRManagementApp.java
@@ -0,0 +1,12 @@
+package com.ebit.hrmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HRManagementApp {
+
+	public static void main(String arg[]) {
+		SpringApplication.run(HRManagementApp.class, arg);
+	}
+}
